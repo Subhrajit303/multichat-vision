c3db3e513c6f06bb2f1c7fb4096aefd7cbcb7a0a
Subhrajit Barman <109448853+Subhrajit303@users.noreply.github.com>
2023-10-13 02:30:37 +0530

    Update server.js
    changes in importing files

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
-const { notFound, errorHandler } = require("./middleware/errorMIddleware");
+const {notFound , errorHandler} = require ('./middleware/errorMiddleware');
 const path = require('path');
 dotenv.config();
 
